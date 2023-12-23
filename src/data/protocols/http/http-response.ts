e8dc2d2376d5488ce480e79ae1d4871b675b46cf
Fábio Almeida <fabioalmeida132@gmail.com>
2023-12-23 13:26:34 -0300

    refactor: feat: ensure RemoteAuthentication throws InvalidCredntialsError on 401

---
+++ src/data/protocols/http/http-response.ts
@@ -0,0 +1,13 @@
+export enum HttpStatusCode {
+  noContent = 204,
+  ok = 200,
+  badRequest = 400,
+  unauthorized = 401,
+  notFound = 404,
+  serverError = 500
+}
+
+export type HttpResponse = {
+  statusCode: HttpStatusCode
+  body?: any
+}
