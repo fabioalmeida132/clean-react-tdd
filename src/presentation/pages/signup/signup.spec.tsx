40e72c568ca8381addde3b52a40e8c311a60ae55
Fábio Almeida <fabioalmeida132@gmail.com>
2024-01-26 20:07:03 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -35,7 +35,7 @@ describe('Signup component', () => {
     Helper.testChildCount(sut, 'error-wrap', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
     Helper.testStatusForField(sut, 'name', validationError)
-    Helper.testStatusForField(sut, 'email', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'email', validationError)
     Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
   })
@@ -47,5 +47,13 @@ describe('Signup component', () => {
     Helper.populateField(sut, 'name')
 
     Helper.testStatusForField(sut, 'name', validationError)
+  })
+  test('Should show email error if Validation fails', () => {
+    const validationError = faker.lorem.words()
+    const { sut } = makeSut({ validationError })
+
+    Helper.populateField(sut, 'email')
+
+    Helper.testStatusForField(sut, 'email', validationError)
   })
 })
