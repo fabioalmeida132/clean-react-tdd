13e71f8a1dbe196ca4bc82af75f6f9a5234d2f45
Fábio Almeida <fabioalmeida132@gmail.com>
2024-01-29 23:30:47 -0300

    test: ensure login save AccessToken if valid credentials are provided

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'cypress'
+import {defineConfig} from 'cypress'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+  env: {
+    realEmail: process.env.REACT_APP_API_EMAIL,
+    realPassword: process.env.REACT_APP_API_PASSWORD
+  },
   e2e: {
     baseUrl: 'http://localhost:8080',
     setupNodeEvents (on, config) {
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,9 +1,13 @@
 import { faker } from '@faker-js/faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+const realEmail = Cypress.env('realEmail')
+const realPassword = Cypress.env('realPassword')
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
+
   it('Should load with correct initial state', () => {
     cy.getByTestId('email').should('have.attr', 'readOnly')
     cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório').should('contain.text', '🔴')
@@ -12,28 +16,45 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
   it('Should present error state if form is invalid', () => {
     const password = faker.lorem.word(4)
     const email = faker.lorem.word()
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.getByTestId('email').focus().type(email)
     cy.getByTestId('email-status').should('have.attr', 'title', 'Valor inválido').should('contain.text', '🔴')
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.getByTestId('password').focus().type(String(password))
     cy.getByTestId('password-status').should('have.attr', 'title', 'Valor inválido').should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
   it('Should present valid state if form is valid', () => {
     const password = faker.lorem.word(5)
     const email = faker.internet.email()
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.getByTestId('email').focus().type(email)
     cy.getByTestId('email-status').should('have.attr', 'title', 'Tudo certo!').should('contain.text', '🟢')
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
     cy.getByTestId('password').focus().type(String(password))
     cy.getByTestId('password-status').should('have.attr', 'title', 'Tudo certo!').should('contain.text', '🟢')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    const password = faker.lorem.word(5)
+    const email = faker.internet.email()
+    cy.getByTestId('email').focus().type(email)
+    cy.getByTestId('password').focus().type(String(password))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
+    cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+  it('Should save AccessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(realEmail)
+    cy.getByTestId('password').focus().type(String(realPassword))
+    cy.getByTestId('submit').click()
+    cy.url().should('eq', `${baseUrl}/`)
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 })
